09bf2edbf981e6f440890dc7307f17523b83444f
JorgeCenzano <jorge.cenzano@alumnos.ucn.cl>
2022-07-19 06:34:30 -0400

    actualizacion

---
+++ app/PicTwinAPIRest.h
@@ -0,0 +1,16 @@
+//
+// Created by Tote on 19-07-2022.
+//
+
+#ifndef PICTWIN_PICTWINAPIREST_H
+#define PICTWIN_PICTWINAPIREST_H
+
+
+
+class PicTwinAPIRest {
+
+};
+
+
+
+#endif //PICTWIN_PICTWINAPIREST_H
